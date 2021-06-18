eeee25c6b64469eff1b7342ba02acb934cca6615
Abhishek Cherath <abhicherath@gmail.com>
2021-06-18 17:06:17 -0400

    modified stuff to work with vercel

--- benchmarks/singlethreaded-rustwasm/do_work.sh
+++ benchmarks/singlethreaded-rustwasm/do_work.sh
@@ -1,4 +1,4 @@
-wasm-pack build --target web
+npx wasm-pack build --target web
 sed '/import\W/d;s/^export//g;/default/d' pkg/Mandelbrot.js > tmp_mandel_import.js
 cat tmp_mandel_import.js  mandel_src.js > Mandelbrot.js
 rm tmp_mandel_import.js
