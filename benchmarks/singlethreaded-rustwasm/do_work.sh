a470411676a15f5fd0ead4cb3b5ac027c41aa037
Abhishek Cherath <abhicherath@gmail.com>
2021-06-18 17:54:08 -0400

    change makefile and script

--- benchmarks/singlethreaded-rustwasm/do_work.sh
+++ benchmarks/singlethreaded-rustwasm/do_work.sh
@@ -1,4 +1,3 @@
-npx wasm-pack build --target web
 sed '/import\W/d;s/^export//g;/default/d' pkg/Mandelbrot.js > tmp_mandel_import.js
 cat tmp_mandel_import.js  mandel_src.js > Mandelbrot.js
 rm tmp_mandel_import.js
