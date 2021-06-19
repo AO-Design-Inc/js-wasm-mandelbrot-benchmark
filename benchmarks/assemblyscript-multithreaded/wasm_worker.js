9e8aa29a9c887e6765056eb019e2f815588ebcda
Abhishek Cherath <abhicherath@gmail.com>
2021-06-18 22:35:53 -0400

    actually fixed caching, hopefully.

--- benchmarks/assemblyscript-multithreaded/wasm_worker.js
+++ benchmarks/assemblyscript-multithreaded/wasm_worker.js
@@ -1,25 +1,23 @@
-const mandel_promise = fetch('build/mandel_final.wasm');
-
 onmessage = ({data}) => {
     const {n_worker, n_threads, memory, width, height, ITER_CONST, START_X_TOTAL, START_Y_TOTAL, WINDOW} = data;
     WebAssembly.instantiateStreaming( 
-    mandel_promise, {
-		env: {
-            abort: () => console.log("Abort!"),
-            memory: memory,
-            log: (message) => console.log(message)
-        },
-        mandel_final: {
-            canvas_width: width,
-            canvas_height: height,
-            DIV_CLASS: n_worker,
-            N_THREADS: n_threads,
-            ITER_CONST,
-            START_X_TOTAL,
-            START_Y_TOTAL,
-            WINDOW
-        }
-    }).then(results => {
-        postMessage("done")
-    })
+	    fetch('build/mandel_final.wasm', {cache: "force-cache"}), {
+		    env: {
+			    abort: () => console.log("Abort!"),
+			    memory: memory,
+			    log: (message) => console.log(message)
+		    },
+		    mandel_final: {
+			    canvas_width: width,
+			    canvas_height: height,
+			    DIV_CLASS: n_worker,
+			    N_THREADS: n_threads,
+			    ITER_CONST,
+			    START_X_TOTAL,
+			    START_Y_TOTAL,
+			    WINDOW
+		    }
+	    }).then(results => {
+		    postMessage("done")
+	    })
 }
