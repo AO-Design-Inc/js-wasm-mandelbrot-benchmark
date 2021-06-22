489e5e354d19898ed4b8e6347b9a544e1c37cd0d
MaxGraey <maxgraey@gmail.com>
2021-06-22 22:10:21 +0300

    improve rust and simple AS variants

--- benchmarks/simple-assemblyscript/assembly/main.ts
+++ benchmarks/simple-assemblyscript/assembly/main.ts
@@ -7,69 +7,64 @@ declare const WINDOW:f32
 
 @unmanaged
 class Complex {
-	real: f64 = 0;
-	imag: f64 = 0;
-
-	constructor(real:f64, imag:f64){
-		this.real = real;
-		this.imag = imag;
-	}
+	@inline constructor(
+		public real: f64 = 0,
+		public imag: f64 = 0
+	) {}
 
 	@inline
-	add(cplx: Complex): Complex {
-		this.real = this.real + cplx.real;
-		this.imag = this.imag + cplx.imag;
+	add(cplx: Complex): this {
+		this.real += cplx.real;
+		this.imag += cplx.imag;
 		return this;
 	}
 
 	@inline
 	mag(): f64 {
-		return Math.sqrt(this.real * this.real + this.imag * this.imag)
+		let re = this.real, im = this.imag;
+		return Math.sqrt(re * re + im * im);
 	}
 
 	@inline
-	mul(cplx: Complex): Complex {
-		const __tempr  = this.real*cplx.real - this.imag*cplx.imag;
-		const __tempi = this.imag*cplx.real + this.real*cplx.imag;
-		this.real = __tempr;
-		this.imag = __tempi;
-		return this
+	mul(cplx: Complex): this {
+		let re1 = this.real, im1 = this.imag;
+		let re2 = this.real, im2 = this.imag;
+		this.real = re1 * re2 - im1 * im2;
+		this.imag = im1 * re2 + re1 * im2;
+		return this;
 	}
 
-
-	set(real:f64,imag:f64): void {
+	@inline
+	set(real: f64, imag: f64): void {
 		this.real = real;
 		this.imag = imag;
 	}
-
 }
 
-const z:Complex = new Complex(0,0);
-const cplx:Complex = new Complex(0,0)
+const z = new Complex(0,0);
+const cplx = new Complex(0,0);
 
 @inline
-function mandelbrot(real:f64,imag:f64):i16{
-	z.set(0,0)
-	cplx.set(real,imag)
+function mandelbrot(real: f64, imag: f64): u16 {
+	z.set(0, 0);
+	cplx.set(real, imag);
 
-	let count:i16 = 0;
-	for (; z.mag() <= 2; count++) {
-		(z.mul(z)).add(cplx); // z = z^2 + cplx
-		if (count > ITER_CONST) {
-			break;
-		}
+	let count = 0;
+	while (z.mag() <= 2 && count < ITER_CONST) {
+		z.mul(z).add(cplx); // z = z^2 + cplx
+		count++;
 	}
-	return count;
+	return count as u16;
 }
 
 export function compute(): void {
-	let memcounter = 0
-	const step_X:f32 = WINDOW/f32(canvas_width);
-	const step_Y:f32 = WINDOW/f32(canvas_height);
-	for (let y = START_Y_TOTAL, count_y = 0; count_y < canvas_height; y += step_Y, count_y++){
-		for (let x = START_X_TOTAL, count_x = 0; count_x < canvas_width; x += step_X, count_x++){
-			store<i16>(memcounter, mandelbrot(x,y));
-			memcounter += 2
+	let memcounter = 0;
+	const step_X:f32 = WINDOW / f32(canvas_width);
+	const step_Y:f32 = WINDOW / f32(canvas_height);
+	for (let y = START_Y_TOTAL, count_y = 0; count_y < canvas_height; y += step_Y, count_y++) {
+		for (let x = START_X_TOTAL, count_x = 0; count_x < canvas_width; x += step_X, count_x++) {
+			store<u16>(memcounter, mandelbrot(x, y));
+			memcounter += 2;
 		}
 	}
 }
--- benchmarks/singlethreaded-rustwasm/src/lib.rs
+++ benchmarks/singlethreaded-rustwasm/src/lib.rs
@@ -3,7 +3,7 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use js_sys::{Uint8ClampedArray, WebAssembly};
 
-// neat macro wrapper for js console.log 
+// neat macro wrapper for js console.log
 #[allow(unused_macros)]
 macro_rules! log {
     ( $( $t:tt )* ) => {
@@ -17,6 +17,7 @@ struct Complex {
     imag: f64,
 }
 
+#[inline(always)]
 fn build_complex(real: f64, imag: f64) -> Complex {
     Complex {
         real,
@@ -26,29 +27,33 @@ fn build_complex(real: f64, imag: f64) -> Complex {
 
 // methods for complex numbers
 impl Complex {
+    #[inline(always)]
     fn add(mut self, other: &Complex) -> Complex {
         self.real += other.real;
         self.imag += other.imag;
         self
     }
 
+    #[inline(always)]
     fn mag(&self) -> f64 {
-        self.real.hypot(self.imag)
+        (self.real * self.real + self.imag * self.imag).sqrt()
     }
 
+    #[inline(always)]
     fn square(mut self) -> Complex {
         // foil operation to multiply complex numbers
-        let real = self.real*self.real - self.imag*self.imag;
-        let imag = self.imag*self.real + self.real*self.imag;
+        let real = self.real * self.real - self.imag * self.imag;
+        let imag = self.imag * self.real + self.real * self.imag;
         self.real = real;
         self.imag = imag;
         self
     }
-
 }
 
-fn in_mandelbrot(cplx: &Complex) -> u8 {
+#[inline(always)]
+fn mandelbrot(cplx: &Complex) -> u8 {
     const ITER_CONST: i32 = 1000;
+
     let mut z = build_complex(0.0, 0.0);
     let mut count: i32 = 0;
 
@@ -66,40 +71,33 @@ fn in_mandelbrot(cplx: &Complex) -> u8 {
 }
 
 fn fill_mandelbrot(
-    points_array: & mut Vec<u8>, 
-    start_x: f64, 
-    start_y: f64, 
-    x_len: i32, 
-    y_len: i32, 
-    window: f64) {
+    points_array: &mut Vec<u8>,
+    start_x: f64,
+    start_y: f64,
+    x_len: i32,
+    y_len: i32,
+    window: f64
+) {
+    let x_step = window / (x_len as f64);
+    let y_step = window / (y_len as f64);
 
-    let num_pixels = x_len * y_len;
+    for y in 0..y_len {
+		for x in 0..x_len {
+            // real and imaginary parts of complex number for
+            // specified canvas pixel
+            let x_com = start_x + x as f64 * x_step;
+            let y_com = start_y + y as f64 * y_step;
 
-    let x_step = window/(x_len as f64);
-    let y_step = window/(y_len as f64);
+            let in_set = mandelbrot(&build_complex(x_com, y_com));
 
-    for count in 0..num_pixels {
-        let c = count as i32;
+            let i = 4 * (y * x_len + x) as usize;
 
-        // calculate coordinates on canvas
-        let x = (c % x_len) as f64;
-        let y = (c / x_len) as f64;
-
-        // real and imaginary parts of complex number for 
-        // specified canvas pixel
-        let x_com = start_x + x * x_step;
-        let y_com = start_y + y * y_step;
-
-        let in_set = in_mandelbrot(&build_complex(x_com, y_com));
-
-        let i = count as usize;
-
-        points_array[i*4] = in_set;
-        points_array[i*4 + 1] = in_set;
-        points_array[i*4 + 2] = in_set;
-        points_array[i*4 + 3] = 255;
+            points_array[i + 0] = in_set;
+            points_array[i + 1] = in_set;
+            points_array[i + 2] = in_set;
+            points_array[i + 3] = 255;
+        }
     }
-
 }
 
 // lets us make an ImageData
@@ -112,14 +110,14 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn run(start_x: f64, start_y: f64, width: u32, height: u32, window: f64) -> ImageData{
+pub fn run(start_x: f64, start_y: f64, width: u32, height: u32, window: f64) -> ImageData {
 
     let y_len: i32 = height as i32;
     let x_len: i32 = width as i32;
 
     let pixels: usize = (x_len * y_len) as usize;
 
-    let mut points_array = vec![0; pixels*4];
+    let mut points_array = vec![0; pixels * 4];
     fill_mandelbrot(&mut points_array, start_x, start_y, x_len, y_len, window);
 
     let pointer = points_array.as_ptr() as usize;
